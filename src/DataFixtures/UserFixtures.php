485b89e9dddee43b07d31607327bc9464ad14e17
junpeko5 <junpeko5@gmail.com>
2019-08-11 08:58:22 +0900

    ナビゲーションバーの修正

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,18 +5,37 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+
+    /**
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i < 2; $i++) {
-            $User = new User();
-            $User->setEmail('junpeko+'.$i.'@gmail.com');
-            $User->setPassword('password');
-            $User->setRoles([User::ROLE_ADMIN]);
-            $manager->persist($User);
-        }
+        $User = new User();
+        $User->setEmail('brad@gmail.com');
+        $password = $this->passwordEncoder->encodePassword($User, 'password');
+        $User->setPassword($password);
+        $User->setRoles([User::ROLE_ADMIN]);
+        $manager->persist($User);
+        $manager->flush();
+
+        $User = new User();
+        $User->setEmail('jun@gmail.com');
+        $password = $this->passwordEncoder->encodePassword($User, 'password');
+        $User->setPassword($password);
+        $User->setRoles([User::ROLE_USER]);
+        $manager->persist($User);
         $manager->flush();
     }
 }
