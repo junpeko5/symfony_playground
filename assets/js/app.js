b7d745c8e9c498307bd80a445e9563516f297a8e
junpeko5 <junpeko5@gmail.com>
2019-08-10 22:22:11 +0900

    bootstrapのインストール

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,11 +7,18 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+require('bootstrap/dist/css/bootstrap.min.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
+require('bootstrap/dist/js/bootstrap.bundle.min.js');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
-$(document).ready(function() {
-  $('body').prepend('<h1>hello jQuery!!!!!!!!!</h1>');
-});
+// console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// $(document).ready(function() {
+//   $('body').prepend('<h1>hello jQuery!!!!!!!!!</h1>');
+// });
+//
+// require('bootstrap/js/dist/popover');
+// $(document).ready(function() {
+//   $('[data-toggle="popover"]').popover();
+// });
