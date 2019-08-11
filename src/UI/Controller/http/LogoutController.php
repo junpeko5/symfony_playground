313ad96ea2632377453c513defee9f462fece84e
junpeko5 <junpeko5@gmail.com>
2019-08-11 10:15:12 +0900

    ログアウト時の設定を追加

--- src/UI/Controller/http/LogoutController.php
+++ src/UI/Controller/http/LogoutController.php
@@ -3,14 +3,12 @@
 namespace App\UI\Controller\http;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LogoutController extends AbstractController
 {
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="app_logout", methods={"GET"})
      */
     public function logout()
     {
