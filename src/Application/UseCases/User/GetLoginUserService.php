868398c1ca278cf29e5aa0094ced9e9d8ef869ae
junpeko5 <junpeko5@gmail.com>
2019-08-12 11:07:53 +0900

    ログインサービスをリファクタリング

--- src/Application/UseCases/User/GetLoginUserService.php
+++ src/Application/UseCases/User/GetLoginUserService.php
@@ -1,16 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Application\UseCases\User;
 
-use App\Domain\User\Model\User;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class LoginUserService
+class GetLoginUserService
 {
+    /**
+     * @var AuthenticationUtils
+     */
     private $authenticationUtils;
 
+    /**
+     * @var string | null
+     */
     private $error;
 
+    /**
+     * @var string
+     */
     private $lastUserName;
 
     public function __construct(AuthenticationUtils $authenticationUtils)
@@ -25,12 +35,18 @@ class LoginUserService
         $this->lastUserName = $this->authenticationUtils->getLastUsername();
     }
 
+    /**
+     * @return string | null
+     */
     public function getError()
     {
         return $this->error;
     }
 
-    public function getLastUserName()
+    /**
+     * @return string
+     */
+    public function getLastUserName() :string
     {
         return $this->lastUserName;
     }
--- src/UI/Controller/http/LoginController.php
+++ src/UI/Controller/http/LoginController.php
@@ -2,33 +2,36 @@
 
 namespace App\UI\Controller\http;
 
-use App\Application\UseCases\User\LoginUserService;
+use App\Application\UseCases\User\GetLoginUserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+    private $useCase;
+
+    public function __construct(GetLoginUserService $useCase)
+    {
+        $this->useCase = $useCase;
+    }
+
     /**
      * @Route("/login", name="admin_login")
-     * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(): Response
     {
-         if ($this->getUser()) {
+        if ($this->getUser()) {
             return $this->redirectToRoute('home_page');
-         }
+        }
 
-        $loginService = new LoginUserService($authenticationUtils);
-
-        $loginService->execute();
+        $this->useCase->execute();
 
         return $this->render('frontend/security/login.html.twig',
             [
-                'last_username' => $loginService->getLastUserName(),
-                'error' => $loginService->getError(),
+                'last_username' => $this->useCase->getLastUserName(),
+                'error' => $this->useCase->getError(),
             ]
         );
     }
